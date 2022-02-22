307ec39d354acf8d1758362dd06b208d82f93a45
amin <aminqanbarii1992@gmail.com>
2022-02-22 09:03:23 -0800

    create logoutHandler and chatEngine

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 //css
-import styles from "./Chats.module.css"
+import styles from "./Chats.module.css";
 
 //components
-import Navbar from './Navbar'
-
+import Navbar from "./Navbar";
+import { ChatEngine } from "react-chat-engine";
 
 const Chats = () => {
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    auth.signOut();
+    navigate("/");
+  };
+
   return (
     <div className={styles.container}>
-        <Navbar/>
+      <Navbar logoutHandler={logoutHandler} />
+      <ChatEngine
+        height="calc(100vh - 50px)"
+        projectID="7eb84ea2-46e4-4e57-b554-599e15413ccb"
+        userName="."
+        userSecret="."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 //css
 import styles from "./Navbar.module.css"
-const Navbar = () => {
+const Navbar = ({logoutHandler}) => {
   return (
     <div className={styles.container}>
         <div className={styles.name}>Amingram</div>
-        <div className={styles.logout}>Logout</div>
+        <div className={styles.logout} onClick={logoutHandler} >Logout</div>
     </div>
   )
 }
